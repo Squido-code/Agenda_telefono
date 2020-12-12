367e92a4182779846ec3b1310f80672ce9cda826
Squido <squidocode@hotmail.com>
2020-12-12 18:33:41 +0100

    Creados métodos para registrar usuario y borrar registros, pero no enlace a botones.

--- src/main/java/com/guillermo/agenda/controllers/APPController.java
+++ src/main/java/com/guillermo/agenda/controllers/APPController.java
@@ -5,13 +5,11 @@ import com.guillermo.agenda.DAO.TelefonoDAO;
 import com.guillermo.agenda.beans.Persona;
 import com.guillermo.agenda.beans.Telefono;
 import com.guillermo.agenda.util.Herramientas;
+import com.sun.javafx.charts.Legend;
 import javafx.collections.FXCollections;
 import javafx.event.Event;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
 
 
 import java.sql.SQLException;
@@ -28,13 +26,24 @@ public class APPController {
     public Label lbTelefono1;
     public Label lbTelefono2;
     public TextArea txNotas;
-    public Button btNuevo;
+    public Button btNuevoContacto;
+    public Button BorrarRegistro;
     public Button btEditar;
     public Button btEliminar;
     public Button btBuscar;
     private Herramientas tool;
     private PersonaDAO persona_dao;
     private TelefonoDAO telefonoDao;
+    private TextField txNNombre;
+    private TextField txNApellido;
+    private TextField txNdireccion;
+    private TextField txNcp;
+    private TextField txNpoblacion;
+    private TextField txNtf1Nombre;
+    private TextField txNtf1Numero;
+    private TextField txNtf2Nombre;
+    private TextField txNtf2Numero;
+
 
     public APPController() {
         tool = new Herramientas();
@@ -84,7 +93,44 @@ public class APPController {
     }
     @FXML
     public void nuevoContacto(){
+        PersonaDAO pdao = new PersonaDAO();
+        Persona p = new Persona();
+        Telefono t1 = new Telefono();
+        Telefono t2 = new Telefono();
+        ArrayList<Telefono> listaT = new ArrayList<>();
+        p.setNombre(txNNombre.getText());
+        p.setApellidos(txNApellido.getText());
+        p.setDireccion(txNdireccion.getText());
+        p.setCodigo_postal(txNcp.getText());
+        p.setPoblacion(txNpoblacion.getText());
+        t1.setNombre(txNtf1Nombre.getText());
+        t1.setNumero(txNtf1Numero.getText());
+        t2.setNombre(txNtf2Nombre.getText());
+        t2.setNumero(txNtf2Numero.getText());
+        listaT.add(t1);
+        listaT.add(t2);
+        p.setTelefono(listaT);
+        try {
+            pdao.insertar(p);
+        } catch (SQLException throwables) {
+            tool.alertaError("Error al conectar con la base de datos");
+        }
+        Alert alerta = new Alert(Alert.AlertType.INFORMATION);
+        alerta.setContentText("Contacto agregado correctamente");
+        cargarDatos();
+        borrarRegistro();
+    }
 
+    private void borrarRegistro() {
+        txNNombre.setText("");
+        txNApellido.setText("");
+        txNdireccion.setText("");
+        txNcp.setText("");
+        txNpoblacion.setText("");
+        txNtf1Nombre.setText("");
+        txNtf1Numero.setText("");
+        txNtf2Nombre.setText("");
+        txNtf2Numero.setText("");
     }
 
 }
